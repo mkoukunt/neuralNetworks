cd9259fed46f77c0880de9c0047af96a23bc5e9d
mkoukunt <mkoukunt@gmail.com>
2026-07-26 21:28:00 -0700

    Transformerencoder added

---
+++ TransformerEncoderClassifier.py
@@ -0,0 +1,113 @@
+import torch
+import torch.nn as nn
+
+# Model parameters.
+EMBED_DIM = 256
+NUM_ENCODER_LAYERS = 3
+NUM_HEADS = 4
+
+
+# Training function.
+def train(model, trainloader, optimizer, criterion, device):
+    model.train()
+    print('Training')
+    train_running_loss = 0.0
+    train_running_correct = 0
+    counter = 0
+    for i, data in tqdm(enumerate(trainloader), total=len(trainloader)):
+        counter += 1
+        inputs, labels = data['text'], data['label']
+        inputs = inputs.to(device)
+        labels = torch.tensor(labels, dtype=torch.float32).to(device)
+        optimizer.zero_grad()
+        # Forward pass.
+        outputs = model(inputs)
+        outputs = torch.squeeze(outputs, -1)
+        # Calculate the loss.
+        loss = criterion(outputs, labels)
+        train_running_loss += loss.item()
+        running_correct = count_correct_incorrect(
+            labels, outputs, train_running_correct
+        )
+        train_running_correct += running_correct
+        # Backpropagation.
+        loss.backward()
+        # Update the optimizer parameters.
+        optimizer.step()
+
+    # Loss and accuracy for the complete epoch.
+    epoch_loss = train_running_loss / counter
+    epoch_acc = 100. * (train_running_correct / len(trainloader.dataset))
+    return epoch_loss, epoch_acc
+
+
+# Validation function.
+def validate(model, testloader, criterion, device):
+    model.eval()
+    print('Validation')
+    valid_running_loss = 0.0
+    valid_running_correct = 0
+    counter = 0
+
+    with torch.no_grad():
+        for i, data in tqdm(enumerate(testloader), total=len(testloader)):
+            counter += 1
+            inputs, labels = data['text'], data['label']
+            inputs = inputs.to(device)
+            labels = torch.tensor(labels, dtype=torch.float32).to(device)
+            # Forward pass.
+            outputs = model(inputs)
+            outputs = torch.squeeze(outputs, -1)
+            # Calculate the loss.
+            loss = criterion(outputs, labels)
+            valid_running_loss += loss.item()
+            running_correct = count_correct_incorrect(
+                labels, outputs, valid_running_correct
+            )
+            valid_running_correct += running_correct
+
+    # Loss and accuracy for the complete epoch.
+    epoch_loss = valid_running_loss / counter
+    epoch_acc = 100. * (valid_running_correct / len(testloader.dataset))
+    return epoch_loss, epoch_acc
+
+class EncoderClassifier(nn.Module):
+    def __init__(self, vocab_size, embed_dim, num_layers, num_heads):
+        super(EncoderClassifier, self).__init__()
+        self.emb = nn.Embedding(vocab_size, embed_dim)
+        self.encoder_layer = nn.TransformerEncoderLayer(
+            d_model=embed_dim,
+            nhead=num_heads,
+            batch_first=True
+        )
+        self.encoder = nn.TransformerEncoder(
+            encoder_layer=self.encoder_layer,
+            num_layers=num_layers,
+        )
+        self.linear = nn.Linear(embed_dim, 1)
+        self.dropout = nn.Dropout(0.2)
+
+    def forward(self, x):
+        x = self.emb(x)
+        x = self.encoder(x)
+        x = self.dropout(x)
+        x = x.max(dim=1)[0]
+        out = self.linear(x)
+        return out
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('device:', device)
+
+model = EncoderClassifier(
+   237,
+    embed_dim=EMBED_DIM,
+    num_layers=NUM_ENCODER_LAYERS,
+    num_heads=NUM_HEADS
+).to(device)
+print(model)
+# Total parameters and trainable parameters.
+total_params = sum(p.numel() for p in model.parameters())
+print(f"{total_params:,} total parameters.")
+total_trainable_params = sum(
+    p.numel() for p in model.parameters() if p.requires_grad)
+print(f"{total_trainable_params:,} training parameters.\n")
